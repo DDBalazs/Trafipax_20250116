6a57669814e166d69fda53462564551689248c53
DDBalazs <csokebalazszsolt@gmail.com>
2025-01-23 17:57:23 +0100

    90-es

--- frmstatisztika.Designer.cs
+++ frmstatisztika.Designer.cs
@@ -29,20 +29,22 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbkategoriak = new System.Windows.Forms.ComboBox();
+            this.rbkategoria = new System.Windows.Forms.RadioButton();
+            this.rball = new System.Windows.Forms.RadioButton();
             this.dgadatok = new System.Windows.Forms.DataGridView();
             this.rendszam = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mertseb = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.buntetes = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.rball = new System.Windows.Forms.RadioButton();
-            this.rbkategoria = new System.Windows.Forms.RadioButton();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbmeres = new System.Windows.Forms.Label();
+            this.lbosszeg = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgadatok)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.cbkategoriak);
             this.groupBox1.Controls.Add(this.rbkategoria);
             this.groupBox1.Controls.Add(this.rball);
             this.groupBox1.Location = new System.Drawing.Point(535, 42);
@@ -51,6 +53,47 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Szűrés";
+            // 
+            // cbkategoriak
+            // 
+            this.cbkategoriak.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbkategoriak.FormattingEnabled = true;
+            this.cbkategoriak.Items.AddRange(new object[] {
+            "Nincs gyorshajtás",
+            "1. kategória",
+            "2. kategória",
+            "3. kategória",
+            "4. kategória",
+            "5. kategória"});
+            this.cbkategoriak.Location = new System.Drawing.Point(78, 131);
+            this.cbkategoriak.Name = "cbkategoriak";
+            this.cbkategoriak.Size = new System.Drawing.Size(199, 21);
+            this.cbkategoriak.TabIndex = 2;
+            this.cbkategoriak.Visible = false;
+            this.cbkategoriak.SelectedIndexChanged += new System.EventHandler(this.cbkategoriak_SelectedIndexChanged);
+            // 
+            // rbkategoria
+            // 
+            this.rbkategoria.AutoSize = true;
+            this.rbkategoria.Location = new System.Drawing.Point(31, 92);
+            this.rbkategoria.Name = "rbkategoria";
+            this.rbkategoria.Size = new System.Drawing.Size(194, 17);
+            this.rbkategoria.TabIndex = 1;
+            this.rbkategoria.TabStop = true;
+            this.rbkategoria.Text = "Sebesség kategória választás";
+            this.rbkategoria.UseVisualStyleBackColor = true;
+            this.rbkategoria.CheckedChanged += new System.EventHandler(this.rbkategoria_CheckedChanged);
+            // 
+            // rball
+            // 
+            this.rball.AutoSize = true;
+            this.rball.Location = new System.Drawing.Point(31, 35);
+            this.rball.Name = "rball";
+            this.rball.Size = new System.Drawing.Size(65, 17);
+            this.rball.TabIndex = 0;
+            this.rball.TabStop = true;
+            this.rball.Text = "Összes";
+            this.rball.UseVisualStyleBackColor = true;
             // 
             // dgadatok
             // 
@@ -90,49 +133,33 @@
             this.buntetes.ReadOnly = true;
             this.buntetes.Width = 125;
             // 
-            // rball
+            // lbmeres
             // 
-            this.rball.AutoSize = true;
-            this.rball.Location = new System.Drawing.Point(31, 35);
-            this.rball.Name = "rball";
-            this.rball.Size = new System.Drawing.Size(82, 21);
-            this.rball.TabIndex = 0;
-            this.rball.TabStop = true;
-            this.rball.Text = "Összes";
-            this.rball.UseVisualStyleBackColor = true;
+            this.lbmeres.AutoSize = true;
+            this.lbmeres.ForeColor = System.Drawing.Color.Red;
+            this.lbmeres.Location = new System.Drawing.Point(566, 308);
+            this.lbmeres.Name = "lbmeres";
+            this.lbmeres.Size = new System.Drawing.Size(127, 13);
+            this.lbmeres.TabIndex = 2;
+            this.lbmeres.Text = "Mérések száma: 0 db";
             // 
-            // rbkategoria
+            // lbosszeg
             // 
-            this.rbkategoria.AutoSize = true;
-            this.rbkategoria.Location = new System.Drawing.Point(31, 92);
-            this.rbkategoria.Name = "rbkategoria";
-            this.rbkategoria.Size = new System.Drawing.Size(246, 21);
-            this.rbkategoria.TabIndex = 1;
-            this.rbkategoria.TabStop = true;
-            this.rbkategoria.Text = "Sebesség kategória választás";
-            this.rbkategoria.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Nincs gyorshajtás",
-            "1. kategória",
-            "2. kategória",
-            "3. kategória",
-            "4. kategória",
-            "5. kategória"});
-            this.comboBox1.Location = new System.Drawing.Point(78, 131);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(199, 25);
-            this.comboBox1.TabIndex = 2;
+            this.lbosszeg.AutoSize = true;
+            this.lbosszeg.ForeColor = System.Drawing.Color.Red;
+            this.lbosszeg.Location = new System.Drawing.Point(566, 368);
+            this.lbosszeg.Name = "lbosszeg";
+            this.lbosszeg.Size = new System.Drawing.Size(138, 13);
+            this.lbosszeg.TabIndex = 3;
+            this.lbosszeg.Text = "Bírságok összege: 0 Ft";
             // 
             // frmstatisztika
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.lbosszeg);
+            this.Controls.Add(this.lbmeres);
             this.Controls.Add(this.dgadatok);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
@@ -145,6 +172,7 @@
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgadatok)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -155,8 +183,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn rendszam;
         private System.Windows.Forms.DataGridViewTextBoxColumn mertseb;
         private System.Windows.Forms.DataGridViewTextBoxColumn buntetes;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbkategoriak;
         private System.Windows.Forms.RadioButton rbkategoria;
         private System.Windows.Forms.RadioButton rball;
+        private System.Windows.Forms.Label lbmeres;
+        private System.Windows.Forms.Label lbosszeg;
     }
 }
--- frmstatisztika.cs
+++ frmstatisztika.cs
@@ -14,11 +14,27 @@ namespace Trafipax_20250116
     public partial class frmstatisztika : Form
     {
         List<Buntetes> lista = new List<Buntetes>();
+        void labelek()
+        {
+            int szam = 0;
+            for(int i = 0; i < lista.Count; i++)
+            {
+                szam++;
+            }
+            lbmeres.Text = "Mérések száma: "+szam.ToString()+" db";
+
+            int osszeg = 0;
+            for(int i = 0; i < lista.Count; i++)
+            {
+                osszeg += lista[i].Osszeg;
+            }
+            lbosszeg.Text = "Bírságok összege: " + osszeg.ToString() + " Ft";
+        }
         void betoltes()
         {
             lista.Clear();
             dgadatok.Rows.Clear();
-            FileStream fs = new FileStream("..\\..\\src\\buntetesek.txt", FileMode.Open);
+            FileStream fs = new FileStream("..\\..\\src\\gyorshajtasok.txt", FileMode.Open);
             StreamReader sr = new StreamReader(fs);
 
             while (!sr.EndOfStream)
@@ -37,7 +53,106 @@ namespace Trafipax_20250116
 
         private void frmstatisztika_Load(object sender, EventArgs e)
         {
+            rball.Checked = true;
             betoltes();
+            labelek();
+        }
+
+        private void rbkategoria_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbkategoria.Checked)
+            {
+                cbkategoriak.Visible= true;
+            }
+            else
+            {
+                dgadatok.Rows.Clear();
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                }
+                cbkategoriak.Visible= false;
+            }
+        }
+
+        private void cbkategoriak_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbkategoriak.SelectedIndex == 0)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (lista[i].Mertseb < 100)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else if (cbkategoriak.SelectedIndex == 1)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (100 <= lista[i].Mertseb && lista[i].Mertseb < 120)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else if (cbkategoriak.SelectedIndex == 2)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (120 <= lista[i].Mertseb && lista[i].Mertseb < 135)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else if (cbkategoriak.SelectedIndex == 3)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (135 <= lista[i].Mertseb && lista[i].Mertseb < 150)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else if (cbkategoriak.SelectedIndex == 4)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (150 <= lista[i].Mertseb && lista[i].Mertseb < 165)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else if (cbkategoriak.SelectedIndex == 5)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (165 <= lista[i].Mertseb && lista[i].Mertseb < 180)
+                    {
+                        dgadatok.Rows.Clear();
+                        dgadatok.Rows.Add(lista[i].Rendszam, lista[i].Mertseb, lista[i].Osszeg);
+                    }
+                }
+                labelek();
+            }
+            else
+            {
+                dgadatok.Rows.Clear();
+            }
         }
     }
 }
